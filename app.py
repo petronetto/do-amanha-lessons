a26fe634b87c3c42921ac85da9115c7f9d64d2ea
Juliano Petronetto <juliano.petronetto@gmail.com>
2018-04-14 11:41:05 -0300

    Fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET']) # Verbo HTTP
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
